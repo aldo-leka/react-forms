cc1978ba8b0ecb4e1660dfbb16cd6d22bae6fcde
Aldo <aldo.leka@live.com>
2025-03-18 12:01:33 +0100

    Lesson 259 - Better ux for input validation.

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -13,7 +13,12 @@ export default function StateLogin() {
         password: ''
     });
 
-    const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+    const [didEdit, setDidEdit] = useState({
+        email: false,
+        password: false
+    });
+
+    const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -29,6 +34,18 @@ export default function StateLogin() {
             ...prevValues,
             [identifier]: value
         }));
+
+        setDidEdit(prevEdit => ({
+            ...prevEdit,
+            [identifier]: false
+        }));
+    }
+
+    function handleInputBlur(identifier) {
+        setDidEdit(prevEdit => ({
+            ...prevEdit,
+            [identifier]: true
+        }));
     }
 
     return (
@@ -42,6 +59,7 @@ export default function StateLogin() {
                         id="email"
                         type="email"
                         name="email"
+                        onBlur={() => handleInputBlur('email')}
                         onChange={(event) => handleInputChange('email', event.target.value)}
                         value={enteredValues.email}
                     />
